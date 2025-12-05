0f818178914cd5e4052cadb2ff9734216d782ca0
TechAbraao <abraao.cutielips@hotmail.com>
2025-12-05 17:37:41 -0300

    feat: secret number game'

--- secret-number/guessSecretNumber.sh
+++ secret-number/guessSecretNumber.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
 
+secret_number=$(( RANDOM % 11 ))
+count=2
+
+echo "Seja bem-vindo ao Acerte o Número Secreto!"
+while true
+do
+    echo "Você tem apenas $((count+1)) tentativas."
+    read -p "Digite um número entre 0 até 10: " number
+    if [ $number -eq $secret_number ]; then
+        echo "Parabéns, você acertou o número secreto."
+        break
+    elif [ $count -eq 0 ]; then
+        echo "Errou todas as tentativas."
+        break
+    elif [ $number -lt $secret_number ]; then
+        echo "O número secreto é um pouco maior."
+        (( count-- ))
+    elif [ $number -gt $secret_number ]; then
+        echo "O número secreto é um pouco menor."
+        (( count-- ))
+    fi
+done
